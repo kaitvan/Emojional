9f69e43e669475b6986e989e2c7b9bcffacdab9a
Kaitlyn Van Gorkom <kaitlyn.vangorkom@gmail.com>
2021-05-12 20:08:40 -0500

    Completed iteration 3 and random messages with different array lengths

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -9,7 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let availableEmotions = ["🥳": "Don't be moody. Shake your booty!", "🙂": "When you focus on the good, the good gets better.", "😞": "I am worthy of the time it takes to do the things that heal my heart."]
+    let availableEmotions = ["🥳": "party", "🙂": "happy", "😞": "sad"]
+    
+    let customMessages = [
+        "party": ["Don't be moody. Shake your booty!", "Well behaved women rarely make history.", "Make your own magic."],
+        "happy": ["When you focus on the good, the good gets better.", "Stay close to people who feel like sunshine."],
+        "sad": ["I am worthy of the time it takes to do the things that heal my heart.", "And I will keep going.", "Only when we are brave enough to explore the darkness will we discover the infinite power of our light."]
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,9 +27,13 @@ class ViewController: UIViewController {
             
             let titleText = selectedEmotion
             
-            let messageText = availableEmotions[selectedEmotion]!
+            let length = customMessages[availableEmotions[selectedEmotion]!]!.count
+            
+            let index = Int.random(in: 0..<length)
+            
+            let selectedMessage = customMessages[availableEmotions[selectedEmotion]!]![index]
         
-            let messageControl = UIAlertController(title: "\(titleText)", message: "\(messageText)", preferredStyle: UIAlertController.Style.alert)
+            let messageControl = UIAlertController(title: "\(titleText)", message: "\(selectedMessage)", preferredStyle: UIAlertController.Style.alert)
         
             messageControl.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         
